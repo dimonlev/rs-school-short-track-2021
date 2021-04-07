58fc391e98e41ad892de4d07b95af4e5261b042a
dimonlev <dimon_lev@mail.ru>
2021-04-07 23:38:29 +0300

    feat: 01-matrix-elements-sum done

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -1,21 +1,37 @@
-/**
- * Given matrix, a rectangular matrix of integers,
- * just add up all the values that don't appear below a "0".
- *
- * @param {Array<Array>} matrix
- * @return {Number}
- *
- * @example
- * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
- * ]
- *
- * The result should be 9
- */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+//   Given matrix, a rectangular matrix of integers,
+//   just add up all the values that don't appear below a "0".
+//  @param {Array<Array>} matrix
+//  @return {Number}
+//   @example
+// const matrix = [
+//   [0, 1, 1, 2],
+//   [0, 5, 0, 0],
+//   [2, 0, 3, 3],
+// ];
+//   The result should be 9
+function getMatrixElementsSum(matrix) {
+  function isZeroAbove(str, ind, matrix2) {
+    let result = true;
+    for (let i = str - 1; i > 0; i--) {
+      if (matrix2[i][ind] === 0) {
+        result = false;
+      }
+    }
+    return result;
+  }
+  let sum = 0;
+  if (matrix[0] === 0) {
+    sum = 0;
+  } else {
+    for (let i = matrix.length - 1; i >= 0; i--) {
+      for (let k = 0; k < matrix[i].length; k++) {
+        if (isZeroAbove(i, k, matrix)) {
+          sum += matrix[i][k];
+        }
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
