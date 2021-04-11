36a53d9c39f2cd79b16c0a41c4277df4b2640c67
dimonlev <dimon_lev@mail.ru>
2021-04-11 13:25:11 +0300

    feat: 12-dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,36 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const count = {};
+  const count2 = {};
+  for (let i = 0; i < domains.length; i++) {
+    const reversed = domains[i].split('.').reverse();
+    // console.log(reversed);
+    let current = reversed[0];
+    if (count[`${current}`]) {
+      count[`${current}`]++;
+    } else {
+      count[`${current}`] = 1;
+    }
+    for (let k = 1; k < reversed.length; k++) {
+      if (count[`${current}.${reversed[k]}`]) {
+        count[`${current}.${reversed[k]}`]++;
+        // console.log(`${current}.${reversed[k]}`);
+        current = `${current}.${reversed[k]}`;
+      } else {
+        count[`${current}.${reversed[k]}`] = 1;
+        // console.log(`${current}.${reversed[k]}`)
+        current = `${current}.${reversed[k]}`;
+      }
+    }
+  }
+  // console.log(Object.keys(count));
+  Object.keys(count).forEach((el) => {
+    count2[`.${el}`] = count[el];
+  });
+  // console.log(count2);
+  return count2;
 }
 
 module.exports = getDNSStats;
